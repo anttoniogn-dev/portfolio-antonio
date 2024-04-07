755b9741977ba00421bfc6dcffe1f39d99b0cdf6
Antonio Santana <antoniosantanagn.dev@gmail.com>
2024-04-07 19:19:16 -0300

    feat::style: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import "@/styles/components/header.css";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header>
-      <span>Header</span>
+    <header className="hdr">
+      <div className="hdr-imgs">
+        <Image src="/icons/gmail-icon.png" alt="gmail" width={20} height={20} />
+        antoniosantanagn.dev@gmail.com
+      </div>
+      <div className="hdr-imgs">
+        <Image
+          src="/icons/linkedin-icon.png"
+          alt="linkedin"
+          width={20}
+          height={20}
+        />
+        <Link href="https://www.linkedin.com/in/antoniosgn/">
+          <span>Antonio Santana</span>
+        </Link>
+      </div>
+      <div className="hdr-imgs">
+        <Image
+          src="/icons/github-icon.png"
+          alt="github"
+          width={20}
+          height={20}
+        />
+        <Link href="https://github.com/anttoniogn-dev">
+          <span>anttoniogn-dev</span>
+        </Link>
+      </div>
     </header>
   );
 }
